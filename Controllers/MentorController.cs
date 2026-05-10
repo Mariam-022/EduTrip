915a3d247b5332fd023d748cad7cf023c564881e
AhmedmostafaElabdli1 <ahmedmostafaismailelabadly@gmail.com>
2026-05-10 18:56:48 +0300

    this is mentor controller

---
+++ Controllers/MentorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EduTrip.Controllers
+{
+    public class MentorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
